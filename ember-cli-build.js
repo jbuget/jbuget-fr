53d145da24f262577ad0d32a2b43e0e6c0634086
Jérémy Buget <jbuget@gmail.com>
2018-09-04 01:14:23 +0200

    Update site

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,9 @@ module.exports = function(defaults) {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
       'importBootstrapCSS': false
+    },
+    sassOptions: {
+      implementation: require('sass')
     }
   });
 
@@ -23,6 +26,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  //app.import('node_modules/hamburgers/dist/hamburgers.css');
 
   return app.toTree();
 };
